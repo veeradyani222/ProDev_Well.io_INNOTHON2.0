835a55799ca150db6e524bd92541b0aa724e15f0
veeradyani222 <167308256+veeradyani222@users.noreply.github.com>
2025-05-10 04:38:55 +0530

    needed

--- wellio.backend/index.js
+++ wellio.backend/index.js
@@ -33,7 +33,7 @@ app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors());
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://veeradyani2:S%40nju_143@cluster0.uafyz.mongodb.net/Doord?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://veeradyani2:S%40nju_143@cluster0.uafyz.mongodb.net/wellio?retryWrites=true&w=majority');
 
 
 app.get("/", (req, res) => {
